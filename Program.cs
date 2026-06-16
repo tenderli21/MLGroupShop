9154d8320ea6db4789e783aa95ee028324ce36ad
tenderli21 <goryanka.ru@gmail.com>
2026-06-16 18:53:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,8 +24,7 @@ var launchUrl = launchUrls.FirstOrDefault(url =>
 var useHttpsRedirection = isDevelopment;
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+   options.UseSqlite("Data Source=shop.db");
 
 builder.Services.AddControllersWithViews();
 
